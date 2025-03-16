33a09ffe6d9cec5b72bbdb525753eafe81788c31
Hanmsoe17 <hanminsoe356@gmail.com>
2025-03-16 13:17:27 +0630

    update Essay and FlashCard

--- app.py
+++ app.py
@@ -3,16 +3,22 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    return render_template('home.html')
 
-@app.route('/priceTag')  # The URL route is `/priceTag`
-def prices():  # The function name is `prices`
-    return render_template('priceTag.html')
-
-@app.route('/teams') 
-def teams():  
+@app.route('/team')
+def teams():
     return render_template('team.html')
+
+
+@app.route('/essay')
+def Essay():
+    return render_template('Essay.html')
+
+
+@app.route('/flashcard')
+def FlashCard():
+    return render_template('FlashCard.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
